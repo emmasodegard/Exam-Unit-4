2b28dc6dc102f23c04b4c8567ec7436749aa02a5
Emma Sunde Ødegård <emmasodegard@gmail.com>
2025-04-08 22:00:56 +0200

    step 3#

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 import Game from './models/Game.mjs';
+
+function saveGame(game) {
+  localStorage.setItem(game.title, JSON.stringify(game));
+}
+
+function getAllGames() {
+  const games = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const gameData = JSON.parse(localStorage.getItem(key));
+    games.push(gameData);
+  }
+  return games;
+}
+
+function exportGames() {
+  const games = getAllGames();
+  return JSON.stringify(games, null, 2);
+}
+
+function importGames(json) {
+  const games = JSON.parse(json);
+  games.forEach(game => {
+    saveGame(game);
+  });
+}
